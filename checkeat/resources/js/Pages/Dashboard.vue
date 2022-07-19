e0d19de0d34a53e8309144da3031c7cabfbddbed
Gabriel Amadé <gabrielamade@icloud.com>
2022-07-19 13:55:15 +0200

    modif

--- checkeat/resources/js/Pages/Dashboard.vue
+++ checkeat/resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
-import { Head } from "@inertiajs/inertia-vue3";
+import { Head, Link } from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
@@ -16,8 +16,20 @@ import { Head } from "@inertiajs/inertia-vue3";
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        Vous êtes connecté ! <br />
+                    <div
+                        class="p-6 bg-white border-b border-gray-200 flex justify-between"
+                    >
+                        <p class="text-lg font-black">Mes Restaurants</p>
+                        <!-- <button>
+                            <a :href="route('createResto')">Ajouter</a>
+                        </button> -->
+
+                        <Link
+                            :href="route('createResto')"
+                            class="text-lg font-black text-red-700 border-2 border-red-700 rounded p-2"
+                        >
+                            Ajouter
+                        </Link>
                     </div>
                 </div>
             </div>
